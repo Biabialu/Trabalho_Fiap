87ca50b66cd602fa822ed35cf20ac0f2344d6887
Beatriz Luiza <beatrizluizar@hotmail.com>
2025-03-29 05:23:42 -0300

    Poesia do Dia

--- ola_mundo.py
+++ ola_mundo.py
@@ -8,3 +8,4 @@ elif nota_satisfacao >=70 and nota_satisfacao < 89 :
             print('Sua satisfação é importante para nós! Vamos continuar trabalhando para melhorar ainda mais a sua experiência. Obrigado pelo seu feedback!') #neutro
 else :
     print('Sentimos muito por não atender às suas expectativas. Seu feedback é essencial para melhorarmos. Conte-nos como podemos fazer melhor!') #insatisfatório
+print ( ' O cravo brigou com a rosa ea rosa morreu despedassada')
